cd65b5738917a87ec76ff841eebfd75494d41f7e
avineo99 <avishek.neo99@gmail.com>
2025-03-25 13:31:27 +0530

    add

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,22 +1,57 @@
-$(document).ready(function () { 
-    $(".toggle").click(function () {
-      $("body").addClass("menuToggle");
-    });
-    $(".close, .menuOverlay").click(function () {
-      $("body").removeClass("menuToggle");
-    });
-  
-  });
+$(document).on('click', '.toggle', function () {
+  $("body").addClass("menuToggle");
+});
+$(document).on('click', '.close, .menuOverlay', function () {
+  $("body").removeClass("menuToggle");
+});
 
   $('.testimonial-wrap').slick({
     dots: false,
-    arrows: true,
     infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+
+    ]
   });
 
 
 
-  AOS.init({
-    duration: 1200,
-    once: true
-  });
+  const targetElement = document.querySelector('body');
+
+  const scrollThreshold = 100;
+  
+  function toggleClassOnScroll() {
+    if (window.scrollY > scrollThreshold) {
+      targetElement.classList.add('scrolled');
+    } else {
+      targetElement.classList.remove('scrolled');
+    }
+  }
+  window.addEventListener('scroll', toggleClassOnScroll);
+
+  
